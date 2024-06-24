95253a019792aa9bc129a819e0847ec95c10e1ea
MahmoudHomePC <asus@DESKTOP-I9SSNMT>
2024-06-24 06:39:37 +0300

    setup translate package ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslationService } from './shared/core/i18n/translation.service';
+import { UtilService } from './shared/services/util.service';
+import { LanguagesEnum } from './shared/enums/LanguagesEnum';
+import { locale as arLang } from './shared/core/i18n/ar';
+import { locale as enLang } from './shared/core/i18n/en';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'E-Commerce';
+  lang: string = '';
+  
+  constructor(
+    private _translationService: TranslationService,
+    private _utilService: UtilService) {
+
+    // Set Language
+    let lang = localStorage.getItem("lang");
+    if (lang) {
+      this.lang = lang;
+    } else {
+      this.lang = LanguagesEnum.English
+      localStorage.setItem("lang", this.lang)
+    }
+
+    this._translationService.setLanguage(this.lang);
+    this._utilService.switchLang(this.lang);
+    this._utilService.switchDirection(this.lang);
+    this._translationService.loadTranslations(enLang, arLang);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TranslateModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
