9c5d42f830934f12b14e75e45a63eb6863366736
Mahmoud <nasrmahmoud225@gmail.com>
2024-10-17 23:40:00 +0300

    Add Guard To Login And register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './shared/guard/auth.guard';
+import { ActiveLoginGuard } from './shared/guard/active-login.guard';
 
 const routes: Routes = [
   { path: '', canActivate:[AuthGuard], loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule) },
   { path: 'home', canActivate:[AuthGuard],  loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule) },
   { path: 'products', canActivate:[AuthGuard], loadChildren: () => import('./views/products/products.module').then(m => m.ProductsModule) },
-  { path: 'login', loadChildren: () => import('./views/login/login.module').then(m => m.LoginModule) },
-  { path: 'register', loadChildren: () => import('./views/register/register.module').then(m => m.RegisterModule) },
+  { path: 'login', canActivate:[ActiveLoginGuard], loadChildren: () => import('./views/login/login.module').then(m => m.LoginModule) },
+  { path: 'register',canActivate:[ActiveLoginGuard], loadChildren: () => import('./views/register/register.module').then(m => m.RegisterModule) },
   { path: 'brands', canActivate:[AuthGuard], loadChildren: () => import('./views/brands/brands.module').then(m => m.BrandsModule) },
   { path: 'categories', canActivate:[AuthGuard], loadChildren: () => import('./views/categories/categories.module').then(m => m.CategoriesModule) },
   { path: 'subCategories', canActivate:[AuthGuard], loadChildren: () => import('./views/subcategories/subcategories.module').then(m => m.SubcategoriesModule) },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { UtilService } from './shared/services/util.service';
 import { LanguagesEnum } from './shared/enums/LanguagesEnum';
 import { locale as arLang } from './shared/core/i18n/ar';
 import { locale as enLang } from './shared/core/i18n/en';
+import { SharedService } from './shared/services/shared.service';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,12 @@ import { locale as enLang } from './shared/core/i18n/en';
 export class AppComponent {
   title = 'E-Commerce';
   lang: string = '';
+  isLogin:boolean = false;
   
   constructor(
     private _translationService: TranslationService,
-    private _utilService: UtilService) {
+    private _utilService: UtilService,
+    private _sharedService:SharedService) {
 
     // Set Language
     let lang = localStorage.getItem("lang");
@@ -31,5 +34,14 @@ export class AppComponent {
     this._utilService.switchLang(this.lang);
     this._utilService.switchDirection(this.lang);
     this._translationService.loadTranslations(enLang, arLang);
+
+
+    // Check if user is logged in
+    this._sharedService.userToken.subscribe({
+      next:() =>{
+       if(this._sharedService.userToken.getValue()) this.isLogin = true
+       else this.isLogin = false
+      }
+    })
   }
 }
